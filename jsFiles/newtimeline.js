2a6768a933e954c6d7916d611db8f22be348b50f
jedwards7813 <59661851+jedwards7813@users.noreply.github.com>
2021-12-17 14:14:27 -0500

    moved outside

---
+++ jsFiles/newtimeline.js
@@ -0,0 +1,28 @@
+jsPsych.data.addProperties({
+    date: new Date(),
+    PROLIFIC_PID: jsPsych.data.getURLVariable('subject'),
+});
+
+function MakeTimeline(game) {
+    this.timeline = [
+    game.intro.r1part1, 
+    game.intro.r1part2, 
+    game.intro.r1part3,
+    game.task.round1,
+    game.Qs.round1,
+    game.intro.r2part1,
+    game.intro.r2part2,
+    game.intro.r2part3,
+    game.task.round2,
+    game.Qs.round2,
+    game.Qs.demographics
+    ]
+};
+
+
+var exp = new MakeTimeline(activeGame);
+
+jsPsych.init({
+    timeline: exp.timeline,
+   
+});
